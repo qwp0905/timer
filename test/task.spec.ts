1c2ee13f5f04cf543eece7ea2fb4506cf7ae99a3
qwp0905 <qwp0905@gmail.com>
2025-08-02 21:14:32 +0900

    fix test code for task

--- test/task.spec.ts
+++ test/task.spec.ts
@@ -1,5 +1,3 @@
-import { TimingWheel } from "../src/timing-wheel"
-
 if (!Symbol.dispose) {
   Object.defineProperty(Symbol, "dispose", {
     get() {
@@ -7,6 +5,9 @@ if (!Symbol.dispose) {
     }
   })
 }
+
+import { spawn } from "child_process"
+import { TimingWheel } from "../src/timing-wheel"
 
 describe("Task", () => {
   let timingWheel: TimingWheel
@@ -118,5 +119,129 @@ describe("Task", () => {
 
     jest.advanceTimersByTime(delay)
     expect(callback).not.toHaveBeenCalled()
+  })
+
+  it("should ignore in eventloop when task is unrefed", async () => {
+    jest.useRealTimers()
+    const result = ""
+
+    const p = new Promise((resolve, reject) => {
+      const prc = spawn("yarn", ["ts-node"])
+      let buffered = ""
+      let errors = ""
+      prc.stdout.on("data", (data) => {
+        buffered += data.toString()
+      })
+      prc.stderr.on("data", (data) => {
+        errors += data.toString()
+      })
+      prc.on("close", () => {
+        if (errors) {
+          reject(new Error(errors.trim()))
+        } else {
+          resolve(buffered.trim())
+        }
+      })
+      prc.on("error", (err) => reject(err))
+      prc.stdin.write(`
+import {TimingWheel} from "${__dirname}/../src/timing-wheel"
+const timingWheel = new TimingWheel()
+timingWheel.registerTimeout(() => {
+  console.log("123123123123")
+}, 1000).unref()
+`)
+      prc.stdin.end()
+    })
+
+    await expect(p).resolves.toBe(result)
+  })
+
+  it("should remain on eventloop when task is refed", async () => {
+    jest.useRealTimers()
+    const result = "result"
+
+    const p = new Promise((resolve, reject) => {
+      const prc = spawn("yarn", ["ts-node"])
+      let buffered = ""
+      let errors = ""
+      prc.stdout.on("data", (data) => {
+        buffered += data.toString()
+      })
+      prc.stderr.on("data", (data) => {
+        errors += data.toString()
+      })
+
+      prc.on("close", () => {
+        if (errors) {
+          reject(new Error(errors.trim()))
+        } else {
+          resolve(buffered.trim())
+        }
+      })
+      prc.on("error", (err) => reject(err))
+
+      prc.stdin.write(`
+import {TimingWheel} from "${__dirname}/../src/timing-wheel"
+const timingWheel = new TimingWheel()
+timingWheel.registerTimeout(() => {
+  console.log("${result}")
+}, 1000)
+`)
+      prc.stdin.end()
+    })
+
+    await expect(p).resolves.toBe(result)
+  })
+
+  it("should be true by default", () => {
+    const task = timingWheel.registerTimeout(() => {}, 1000)
+    expect(task.hasRef()).toBe(true)
+    task.close()
+  })
+
+  it("should be false when unrefed", () => {
+    const task = timingWheel.registerTimeout(() => {}, 1000)
+    task.unref()
+    expect(task.hasRef()).toBe(false)
+    task.close()
+  })
+
+  it("should be true when refed", () => {
+    const task = timingWheel.registerTimeout(() => {}, 1000)
+    task.unref()
+    task.ref()
+    expect(task.hasRef()).toBe(true)
+    task.close()
+  })
+
+  it("should not effect when multiple ref/unref called", () => {
+    const task = timingWheel.registerInterval(() => {}, 1000)
+    task.unref()
+    expect(task.hasRef()).toBe(false)
+    task.unref()
+    expect(task.hasRef()).toBe(false)
+    task.unref()
+    expect(task.hasRef()).toBe(false)
+
+    task.ref()
+    expect(task.hasRef()).toBe(true)
+    task.ref()
+    expect(task.hasRef()).toBe(true)
+    task.ref()
+    expect(task.hasRef()).toBe(true)
+
+    task.close()
+  })
+
+  it("should return id typeof number", () => {
+    const task = timingWheel.registerTimeout(() => {}, 1000)
+    expect(task.getId()).not.toBeNaN()
+    task.close()
+  })
+
+  it("should convert to number", () => {
+    const task = timingWheel.registerTimeout(() => {}, 1000)
+    expect(+task).not.toBeNaN()
+    task.close()
   })
 })
