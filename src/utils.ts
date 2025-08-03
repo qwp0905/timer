c0b53ec7935c35738e3b6a51ede78572c8bb7edf
qwp0905 <qwp0905@gmail.com>
2025-08-03 12:37:04 +0900

    fix typo

--- src/utils.ts
+++ src/utils.ts
@@ -3,14 +3,14 @@ const MAX = 0xffff_ffff
 export function convertToIndex(timestamp: number) {
   const indexes: number[] = []
 
+  while (timestamp > MAX) {
+    indexes.push(timestamp % BUCKET_SIZE)
+    timestamp = Math.floor(timestamp / BUCKET_SIZE)
+  }
+
   while (timestamp > 0) {
-    if (timestamp > MAX) {
-      indexes.push(timestamp % BUCKET_SIZE)
-      timestamp = Math.floor(timestamp / BUCKET_SIZE)
-    } else {
-      indexes.push(timestamp & BUCKET_MASK)
-      timestamp >>>= BUCKET_SIZE_BIT
-    }
+    indexes.push(timestamp & BUCKET_MASK)
+    timestamp >>>= BUCKET_SIZE_BIT
   }
 
   return indexes
