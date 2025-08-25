bb855644f78300acb502b7771447dbdfdbb632a3
qwp0905 <qwp0905@github.com>
2025-08-26 08:39:17 +0900

    add vec cap

--- timing-wheel/src/constant.rs
+++ timing-wheel/src/constant.rs
@@ -1,3 +1,5 @@
 pub const BUCKET_SIZE_BIT: usize = 6;
 pub const BUCKET_SIZE: usize = 1 << BUCKET_SIZE_BIT;
 pub const BUCKET_MASK: usize = BUCKET_SIZE - 1;
+pub const MAX_BUCKET_INDEX: usize =
+  (usize::MAX.checked_ilog2().unwrap() as usize).div_ceil(BUCKET_SIZE_BIT);
--- timing-wheel/src/index.rs
+++ timing-wheel/src/index.rs
@@ -1,10 +1,10 @@
-use crate::constant::{BUCKET_MASK, BUCKET_SIZE_BIT};
+use crate::constant::{BUCKET_MASK, BUCKET_SIZE_BIT, MAX_BUCKET_INDEX};
 
 pub type BucketIndexes = Vec<usize>;
 
 #[inline]
 pub fn get_bucket_indexes(scheduled_at: usize) -> Vec<usize> {
-  let mut indexes = Vec::new();
+  let mut indexes = Vec::with_capacity(MAX_BUCKET_INDEX);
   let mut current = scheduled_at;
   while current > 0 {
     indexes.push(current & BUCKET_MASK);
